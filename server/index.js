7bbbc032aa87f93762d6b0305937d8ca071cc1f7
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-05-10 09:41:21 -0400

    beginning set up

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
+const massive = require('massive')
 
 
 const app = express();
 
 app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+})
+    .then(db => {
+        app.set('db', db);
+        console.log('db connected')
+    })
+    .catch(err => console.log(err));
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -20,4 +31,4 @@ app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
